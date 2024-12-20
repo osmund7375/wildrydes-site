ad423ee99df6d1bca9d8d85d792e056a3d15f925
osmund7375 <166950175+osmund7375@users.noreply.github.com>
2024-12-20 17:35:35 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ca-central-1_KLNLI7XYg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '13rh9h0nqtfg2t2veai3nn0f25', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
